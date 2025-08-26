e66bb7088367a53cb530d7ddb9757c3f859a060d
Colopapa001 <jpapa@uade.edu.ar>
2025-08-25 21:50:38 -0300

    update

--- Clase4Node/src/App.jsx
+++ Clase4Node/src/App.jsx
@@ -5,8 +5,6 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const handleProfileClick = () => {
     alert('Navegando al perfil...')
   }
